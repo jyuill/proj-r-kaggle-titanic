941b3cd9765333526ec07372951c3dfe39e844fb
jyuill <john@fig4.com>
2017-11-11 19:05:30 -0800

    followed the instruction on how to predict missing Fare and Age values using regression instead of simple median. submitted to Kaggle: slightly worse score lol

--- titanic2.R
+++ titanic2.R
@@ -4,5 +4,212 @@
 ## builds on data from pt 1
 ## more sophisticated cleaning of data for better results
 
+### THIS SECTION SAME AS titanic1.R ===================================
+## Get training data
+titanic.train <- read.csv("input/train.csv", stringsAsFactors = FALSE)
+str(titanic.train)
+summary(titanic.train)
+head(titanic.train)
 
+library(ggplot2)
+
+plot1 <- ggplot(titanic.train, aes(x=Sex))+geom_bar(stat="count")
+facet1 <- facet_grid(.~Survived)
+facet2 <- facet_grid(Pclass~.~Survived)
+
+plot1
+plot1+facet1
+plot1+facet1+facet2
+plot1+facet2
+
+## Get test data
+titanic.test <- read.csv("input/test.csv", stringsAsFactors = FALSE)
+str(titanic.test)
+summary(titanic.test)
+head(titanic.test)
+
+### Combine data for cleaning
+## check medians
+median(titanic.train$Age, na.rm=TRUE)
+median(titanic.test$Age, na.rm=TRUE)
+## create new column
+titanic.train$IsTrainSet <- TRUE
+titanic.test$IsTrainSet <- FALSE
+## check number of columns
+ncol(titanic.train)
+ncol(titanic.test) ## one col less
+## check column names
+names(titanic.train)
+names(titanic.test)
+
+## add Survived col to test
+titanic.test$Survived <- NA
+
+## new dataset combined
+titanic.full <- rbind(titanic.train, titanic.test)
+418+891 ## check number of rows
+nrow(titanic.full)
+## create table to show number of rows in each category for IsTrainSet
+table(titanic.full$IsTrainSet)
+
+## check cols for missing values (NA)
+summary(titanic.full) ## Age, Fare; strings with missing/empty values may not show up
+
+## fix Embarked
+table(titanic.full$Embarked) ## indicates 2 items with no category
+table(is.na(titanic.full$Embarked)) ## doesn't show as NA
+table(is.null(titanic.full$Embarked)) ## doesn't show as NULL
+table(titanic.full$Embarked=='') ## shows up as ''
+## finding rows with missing values
+titanic.full[titanic.full$Embarked=='',]
+## check distribution of values again to see which is most common
+table(titanic.full$Embarked) ## S is most common (mode)
+## replace missing values with S
+titanic.full[titanic.full$Embarked=='',"Embarked"] <- "S"
+table(titanic.full$Embarked) ## check: no blanks, 2 more 'S'
+
+## fix Age
+## check/clean age
+table(is.na(titanic.full$Age)) ## lots missing
+
+### SIMPLISTIC METHOD REPLACED BY MORE SOPHISTICATED APPROACH BELOW FARE IN CODE BELOW
+## replace with median > very primitive approach but works ok
+#age.replace <- median(titanic.full$Age, na.rm=TRUE)
+#titanic.full[is.na(titanic.full$Age), "Age"] <- age.replace
+#### END OF CODE FROM VERSION 1 =========================================
+
+#### NEW CODE - MORE SOPHISTICATED DATA CLEANING/REPLACEMENT
+## fix Fare
+## in version 1, replaced with median > very primitive approach
+## NOW: more advanced approach, based on regression
+table(is.na(titanic.full$Fare)) ## only 1 missing, so not great example but still useful
+## replace with median > very primitive approach, but works
+##fare.replace <- median(titanic.full$Fare, na.rm=TRUE)
+##titanic.full[is.na(titanic.full$Fare), "Fare"] <- fare.replace
+
+## create regression model to estimate replacement
+boxplot(titanic.full$Fare) ## check for outliers
+## lots of outliers so better to remove
+## check boxplot.stats to see value of whiskers
+boxplot.stats(titanic.full$Fare)$stats 
+## stats shows: min of min or 1.5 IQR below 1st quartile, 1st quartile, median, 3rd quartile, 1.5 IQR frm 3rd quartile
+## 3rd quartile value (upper whisker)
+upper.whisker <- boxplot.stats(titanic.full$Fare)$stats[5]
+## create filter based on 3rd quartile value
+outlier.filter <- titanic.full$Fare < upper.whisker
+## filter data for row numbers that match TRUE from the outlier.filter
+fare.filter <- titanic.full[outlier.filter, ]
+
+## build model to predict fare
+fare.equation <- "Fare ~ Pclass + Sex + Age + SibSp + Parch + Embarked"
+fare.model <- lm(
+  formula = fare.equation,
+  data=fare.filter
+)
+## check model
+summary(fare.model) ## looks like not all variables are helpful, but leaving them in anyway
+
+## subset data for na rows and columns in model to use in predicting missing value
+fare.row <- titanic.full[is.na(titanic.full$Fare), c("Pclass", "Sex", "Age", "SibSp", "Parch", "Embarked")]
+
+## predict fare values based on model
+fare.pred <- predict(fare.model, newdata=fare.row)
+
+## replace NA with predicted value from model
+titanic.full[is.na(titanic.full$Fare), "Fare"] <- fare.pred
+
+## check if replaced value is as expected from the prediction model
+titanic.full[1044,"Fare"]
+
+### USE THE SAME APPROACH AS ABOVE FOR REPLACING MISSING AGE VALUES
+
+## check/clean age
+table(is.na(titanic.full$Age)) ## lots missing
+## get rows for missing values
+age.na <- which(is.na(titanic.full$Age))
+
+## create regression model to estimate replacement
+boxplot(titanic.full$Age) ## check for outliers
+## a few outliers to remove
+## 3rd quartile value (upper whisker)
+upper.whisker <- boxplot.stats(titanic.full$Age)$stats[5]
+## create filter based on 3rd quartile value
+outlier.filter <- titanic.full$Age < upper.whisker
+## filter data for row numbers that match TRUE from the outlier.filter
+age.filter <- titanic.full[outlier.filter, ]
+
+## build model to predict fare
+age.equation <- "Age ~ Pclass + Sex + SibSp + Parch"
+age.model <- lm(
+  formula = age.equation,
+  data=age.filter
+)
+## check model
+summary(age.model) ## not great model but worsens with variables removed
+
+## subset data for na rows and columns in model to use in predicting missing value
+age.row <- titanic.full[is.na(titanic.full$Age), c("Pclass", "Sex", "SibSp", "Parch")]
+
+## predict fare values based on model
+age.pred <- predict(age.model, newdata=age.row)
+
+## replace NA with predicted value from model
+titanic.full[is.na(titanic.full$Age), "Age"] <- age.pred
+
+## check if replaced value is as expected from the prediction model
+titanic.full[c(age.na),"Age"]
+
+## some rows have predicted Age < 0
+titanic.full[titanic.full$Age<0,]
+## replace predicted Age < 0 with 1, on assumption they are likely very young
+titanic.full$Age[titanic.full$Age<0] <- 1
+
+#### ORIGINAL CODE RESUMES ==============================================
+## Run this code to complete the assignment
+
+## categorical casting
+str(titanic.full)
+titanic.full$Pclass <- as.factor(titanic.full$Pclass)
+titanic.full$Sex <- as.factor(titanic.full$Sex)
+titanic.full$Embarked <- as.factor(titanic.full$Embarked)
+str(titanic.full)
+## may also want to cast Sibsp and Parch as.ordinal
+
+### Split data back out to train and test sets
+## clean training set
+titanic.train2 <- titanic.full[titanic.full$IsTrainSet==TRUE,]
+## clean test set -> using dplyr to achieve same as above
+library(dplyr)
+titanic.test2 <- titanic.full %>% filter(IsTrainSet==FALSE)
+
+## Survived as factor
+## convert Survived on training data only - no NAs
+## makes it clear that this is classification problem, not regression, etc
+titanic.train2$Survived <- as.factor(titanic.train2$Survived)
+
+### BUILD PREDICTIVE MODEL
+## select predictors
+survived.equation <- "Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked"
+survived.formula <- as.formula(survived.equation)
+
+library(randomForest)
+titanic.model <- randomForest(formula(survived.formula), data=titanic.train2, ntree=500, mtry=3, nodesize=0.01*nrow(titanic.test2))
+
+## list of features used in model -> not sure how this is used
+features.equation <- "Pclass + Sex + Age + SibSp + Parch + Fare + Embarked"
+
+## make prediction based on the model applied to test data
+Survived <- predict(titanic.model, newdata=titanic.test2)
+## see results of the model
+Survived
+
+## get passenger ids from test set
+PassengerId <- titanic.test2$PassengerId
+## create data frame with passenger ids
+output.df <- as.data.frame(PassengerId)
+## add survived column from prediction above - assumes same order as passenger ids in test2
+output.df$Survived <- Survived 
+
+## save file
+write.csv(output.df, "output/prediction2.csv", row.names = FALSE)
 
